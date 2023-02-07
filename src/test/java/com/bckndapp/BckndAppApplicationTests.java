3c548cd4355335d575469cae6ec7e61b485ab194
Z4jo <ondrataus@seznam.cz>
2023-02-07 22:23:47 +0100

    C

---
+++ src/test/java/com/bckndapp/BckndAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bckndapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BckndAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
